e170fe05f10c1bcd2a9ed1d67c6a731dc3b857ba
beact-akshat <122158329+beact-akshat@users.noreply.github.com>
2023-12-08 10:50:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(
     helmet.contentSecurityPolicy({
         directives: {
             defaultSrc: ["'self'"],
-            connectSrc: ["'self'", 'ad-test-n94k.onrender.com', 'ads.google.com', 'tpc.googlesyndication.com', 'pagead2.googlesyndication.com', 'csi.gstatic.com/csi', 'fonts.googleapis.com/css', 'fonts.googleapis.com/css2', 'vr.google.com/shaders/w/techspecs/'],
+            connectSrc: ["'self'", 'ad-test-n94k.onrender.com', 'ads.google.com', 'https://tpc.googlesyndication.com https://pagead2.googlesyndication.com', 'blob:', 'data:', 'https://csi.gstatic.com/csi', 'https://fonts.googleapis.com/css', 'https://fonts.googleapis.com/css2', 'https://vr.google.com/shaders/w/techspecs/'],
             frameSrc: ["'self'", 'ads.google.com'],
         },
         reportOnly: true,
